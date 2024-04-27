50dc935f368a7ee21a627b70f7d37e88a0cefb80
Imran Khan <imrankhanzemri@gmail.com>
2024-04-27 14:48:46 +0500

    c

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowOrigin", builder =>
     {
-        builder.WithOrigins("https://pwswarehouse.azurewebsites.net")
+        builder
            .AllowAnyOrigin()
             .AllowAnyHeader()
             .AllowAnyMethod();
